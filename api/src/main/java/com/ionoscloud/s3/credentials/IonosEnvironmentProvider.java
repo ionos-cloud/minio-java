283b190d5ecac894d74b5ca87a4b134e042c3703
Radu Mocanu <radu_mocanu2000@yahoo.com>
2024-08-23 16:03:03 +0300

    chore: rename auth env vars to IONOS_S3_ACCESS_KEY and IONOS_S3_SECRET_KEY

--- api/src/main/java/com/ionoscloud/s3/credentials/IonosEnvironmentProvider.java
+++ api/src/main/java/com/ionoscloud/s3/credentials/IonosEnvironmentProvider.java
@@ -5,6 +5,6 @@ public class IonosEnvironmentProvider extends EnvironmentProvider {
   @Override
   public Credentials fetch() {
     return new Credentials(
-        getProperty("IONOS_ACCESS_KEY"), getProperty("IONOS_S3_SECRET_KEY"), null, null);
+        getProperty("IONOS_S3_ACCESS_KEY"), getProperty("IONOS_S3_SECRET_KEY"), null, null);
   }
 }
